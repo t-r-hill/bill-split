35ec4e7e6446d4f99130ee3fffc066fabcf1ec69
Thomas Hill <tomhill555@gmail.com>
2023-04-03 19:42:41 +0100

    second commit

---
+++ src/main/java/com/tomiscoding/billsplit/BillSplitApplication.java
@@ -0,0 +1,13 @@
+package com.tomiscoding.billsplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSplitApplication.class, args);
+	}
+
+}
